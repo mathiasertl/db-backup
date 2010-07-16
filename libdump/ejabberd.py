bdff93aca7c42b8ef6aa2153ac945d76e7635015
mati <mati@a9a8dfd7-e32a-4bca-b1b2-e405d18a79b1>
2010-07-16 14:16:13 +0000

    ups, forgot to actually commit the backend
    git-svn-id: https://svn.fsinf.at/fs/dbdump@63 a9a8dfd7-e32a-4bca-b1b2-e405d18a79b1

---
+++ libdump/ejabberd.py
@@ -0,0 +1,26 @@
+from libdump import backend
+import subprocess, os
+
+class ejabberd( backend.backend ):
+	def get_db_list( self ):
+		return [ 'ejabberd' ]
+
+	def get_command( self, database ):
+		path = os.path.normpath( self.options.base_dir + '/' + database )
+		return [ 'cat', path ]
+		
+	def prepare_db( self, database ):
+		cmd = [ 'ejabberdctl' ]
+		if self.options.node:
+			cmd += [ '--node', self.options.node ]
+		if self.options.auth:
+			cmd.append( '--auth' )
+			cmd += list( self.options.auth )
+
+		cmd += [ 'backup', database + '.backup' ]
+		p = subprocess.Popen( cmd )
+		p.communicate()
+
+	def cleanup_db( self, database ):
+		path = os.path.normpath( self.options.base_dir + '/' + database + '.backup' )
+		os.remove( path )
