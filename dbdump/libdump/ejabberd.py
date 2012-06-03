dd61f82aadec313eba3cbaec81186eb0310817cc
Mathias Ertl <mati@fsinf.at>
2012-06-03 13:02:17 +0200

    prefix ejabberd settings with ejabberd-

--- dbdump/libdump/ejabberd.py
+++ dbdump/libdump/ejabberd.py
@@ -3,23 +3,23 @@ import subprocess, os
 
 class ejabberd(backend.backend):
     def get_db_list(self):
-        return [ 'ejabberd' ]
+        return ['ejabberd']
 
     def get_command(self, database):
-        path = os.path.normpath(self.section['base-dir'] + '/' + database + '.backup')
+        path = os.path.normpath(self.section['ejabberd-base-dir'] + '/' + database + '.backup')
         return [ 'cat', path ]
         
     def prepare_db(self, database):
         cmd = [ 'ejabberdctl' ]
-        if 'node' in self.section:
-            cmd += [ '--node', self.section['node'] ]
-        if 'auth' in self.section:
-            cmd += ['--auth', self.section['auth'].split()]
+        if 'ejabberd-node' in self.section:
+            cmd += [ '--node', self.section['ejabberd-node'] ]
+        if 'ejabberd-auth' in self.section:
+            cmd += ['--auth', self.section['ejabberd-auth'].split()]
 
         cmd += [ 'backup', database + '.backup' ]
         p = subprocess.Popen(cmd)
         p.communicate()
 
     def cleanup_db(self, database):
-        path = os.path.normpath(self.section['base-dir'] + '/' + database + '.backup')
+        path = os.path.normpath(self.section['ejabberd-base-dir'] + '/' + database + '.backup')
         os.remove(path)
