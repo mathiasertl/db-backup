b37718349f79cb4470fbff00244136212bf6ce6e
Mathias Ertl <mati@fsinf.at>
2012-06-03 13:43:12 +0200

    fix missing import

--- dbdump/dbdump.py
+++ dbdump/dbdump.py
@@ -22,7 +22,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os, sys, time, argparse
+import os, sys, time, argparse, configparser
 from libdump import *
 
 config_file = ['/etc/dbdump/dbdump.conf', os.path.expanduser('~/.dbdump.conf')]
