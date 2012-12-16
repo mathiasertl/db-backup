fdcb7249c0e3533fad92fddd6131a137cf328ab4
Mathias Ertl <mati@fsinf.at>
2012-12-16 19:47:41 +0100

    update copyright notices

--- dbdump/libdump/postgresql.py
+++ dbdump/libdump/postgresql.py
@@ -1,7 +1,7 @@
 """
 This file is part of dbdump.
 
-Copyright 2009, 2010 Mathias Ertl
+Copyright 2009-2012 Mathias Ertl
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
